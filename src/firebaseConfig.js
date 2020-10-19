71164151e7fa16087b2d3ec21d74d953d072818d
AndersonAGodoy <anderson.a.godoy@gmail.com>
2020-10-19 10:14:11 -0300

    Update firebaseConfig.js

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,11 +1,5 @@
 const firebaseConfig = {
-  apiKey: "AIzaSyBAinYaYGRbhanCpsuOqpWWJrqvvU9rNwQ",
-  authDomain: "whatsappclone-a999f.firebaseapp.com",
-  databaseURL: "https://whatsappclone-a999f.firebaseio.com",
-  projectId: "whatsappclone-a999f",
-  storageBucket: "whatsappclone-a999f.appspot.com",
-  messagingSenderId: "532571909826",
-  appId: "1:532571909826:web:414541af2e9c7e319d955b"
+ // type your firebaseConfig here
 };
 
 export default firebaseConfig;
